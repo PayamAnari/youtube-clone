a65295bc0041adc09589aed9b8836195d09914ff
PayamAnari <anari.p62@gmail.com>
2024-01-24 20:46:12 +0100

    Create navlayout

---
+++ resources/js/Layouts/NavLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+const showingNavigationDropdown = ref(false);
+</script>
+
+<template>
+    <div>
+      This is a Nav
+      <slot />
+    </div>
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import NavLayout from '@/Layouts/NavLayout.vue';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Youtube" />
+
+    <NavLayout>
+      Welcome
+    </NavLayout>
+
+</template>
+
