77dd94ea529f20ec293aebf0e2d40a0adc09f44a
PayamAnari <anari.p62@gmail.com>
2024-01-25 17:03:07 +0100

    Modify video.vue

--- resources/js/Pages/Video.vue
+++ resources/js/Pages/Video.vue
@@ -20,6 +20,22 @@ import ThumbDownOutline from 'vue-material-design-icons/ThumbDownOutline.vue'
         <div class="xl:flex">
            <div class="p-3">
              <video src="/videos/car.mp4" controls autoplay />
+             <div class="text-white text-2xl font-extrabold mt-4">A cool video</div>
+             <div class="flex items-center mb-4">
+               <img 
+               class="rounded-full m-1.5 mt-2 flex items-baseline w-8 h-8"
+               :src="`https://picsum.photos/id/${(Math.random() * 100).toFixed(0)}/100` || ''"
+               />
+             <div class="pl-2 mt-1">
+                <div class=" text-white text-lg font-extrabold flex items-center">
+                  Payam Anari <CheckCircle fillColor="#888888" :size="17" />
+                </div>
+                 <div class="text-sm text-gray-400 font-extrabold">1k views - 3 days ago</div>
+               </div>
+             </div>
+             <div class="bg-[3F3F3F] rounded-lg w-full p-3 text-white">
+                <div class="text-white text-lg font-extrabold">This a comment</div>
+             </div>
            </div>
         </div>
     </NavLayout>
