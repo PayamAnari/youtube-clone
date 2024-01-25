e6ef4d9c02e9faa98cbcb9ed3bfadce3ae0de5ad
PayamAnari <anari.p62@gmail.com>
2024-01-25 10:46:44 +0100

    Define props in videocard

---
+++ resources/js/Components/VideoCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="relative">
+     <div class="rounded-lg bg-black m-2">
+
+     </div>
+  </div>
+  
+</template>
+
+<script setup>
+import { defineProps, toRefs, ref, watch, onMounted } from 'vue';
+import CheckCircle from 'vue-material-design-icons/CheckCircle.vue';
+
+const props = defineProps({
+  title: String,
+  user: String,
+  views: String,
+  image: String,
+  videoUrl: String,
+  thumbnail: String,
+})
+
+</script>
