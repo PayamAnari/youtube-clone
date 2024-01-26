192211c9a3fe5c876886f693d9cd552ebb51d491
PayamAnari <anari.p62@gmail.com>
2024-01-26 22:54:08 +0100

    update

--- resources/js/Components/RecommendedVideos.vue
+++ resources/js/Components/RecommendedVideos.vue
@@ -1,6 +1,23 @@
 <script setup>
-<
+import { defineProps, toRefs, ref, watch, onMounted } from 'vue';
+import CheckCircle from 'vue-material-design-icons/CheckCircle.vue';
 
+const props = defineProps({ vid: Object });
+const { vid } = toRefs(props);
+
+let show = ref(false);
+let video = ref(null);
+
+watch(() => show.value, (show) => {
+  if (show) {
+    showVideo.value = true
+    video.value.play()
+    return ''
+  }
+  showVideo.value = false
+  video.value.pause()
+  video.value.currentTime = 0
+})
 </script>
 
 <template>
