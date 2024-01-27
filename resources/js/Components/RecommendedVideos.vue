ef3dad3ddcf42b346b0590c08047491018a139bb
PayamAnari <anari.p62@gmail.com>
2024-01-27 20:51:23 +0100

    Modify recommendedvideos

--- resources/js/Components/RecommendedVideos.vue
+++ resources/js/Components/RecommendedVideos.vue
@@ -10,11 +10,9 @@ let video = ref(null);
 
 watch(() => show.value, (show) => {
   if (show) {
-    showVideo.value = true
     video.value.play()
     return ''
   }
-  showVideo.value = false
   video.value.pause()
   video.value.currentTime = 0
 })
