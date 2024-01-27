7d8993d3f6e010feac48c5aeb6470fc3854070ec
PayamAnari <anari.p62@gmail.com>
2024-01-27 21:44:26 +0100

    Modify addvideo

--- resources/js/Pages/AddVideo.vue
+++ resources/js/Pages/AddVideo.vue
@@ -7,8 +7,13 @@ import NavLayout from '@/Layouts/NavLayout.vue';
    <Head title="Youtube" />
 
    <NavLayout>
-    <div class="">
-
+    <div class="max-w-xl mx-auto px-4">
+      <div class="text-white font-extrabold text-3xl py-10">Add Video</div>
+       <form>
+         <div>
+            <div class="text-gray-200">Title</div>
+         </div>
+       </form>
     </div>
    </NavLayout>
 </template>
