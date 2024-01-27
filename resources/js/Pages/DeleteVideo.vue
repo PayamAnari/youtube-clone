2fcde86f3984e4961d32638232e691c99a8ba4ab
PayamAnari <anari.p62@gmail.com>
2024-01-27 21:17:09 +0100

    modify deletevideo

--- resources/js/Pages/DeleteVideo.vue
+++ resources/js/Pages/DeleteVideo.vue
@@ -1,8 +1,27 @@
 <script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import NavLayout from '@/Layouts/NavLayout.vue';
 
 
+// definePorts({
+//     canLogin: {
+//         type: Boolean,
+//     }
+// });
 </script>
 
 <template>
-  
+  <Head title="Youtube" />
+
+   <NavLayout>
+     <div class="max-2xl mx-auto p-4">
+      <table class="min-w-full">
+         <thead class="border-b">
+           <tr class="text-white flex justify-between">
+
+           </tr>
+         </thead>
+      </table>
+     </div>
+   </NavLayout>
 </template>
