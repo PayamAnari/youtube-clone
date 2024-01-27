3c07615f4b166745e5fcd77bb5bfb5b43a0d1e5c
PayamAnari <anari.p62@gmail.com>
2024-01-27 21:48:02 +0100

    Add input to addvideo

--- resources/js/Pages/AddVideo.vue
+++ resources/js/Pages/AddVideo.vue
@@ -12,6 +12,32 @@ import NavLayout from '@/Layouts/NavLayout.vue';
        <form>
          <div>
             <div class="text-gray-200">Title</div>
+             <input
+             type="text"
+             class="
+             form-control
+             block
+             w-full
+             px-5
+             py-1.5
+             text-xl
+             font-normal
+             text-gray-200
+             bg-black
+             placeholder-gray-400
+             bg-clip-padding
+             border
+             border-bold
+             border-gray-600
+             rounded
+             transition
+             ease-in-out m-0
+             border-transparent
+             focus:ring-0
+
+
+             "
+             >
          </div>
        </form>
     </div>
