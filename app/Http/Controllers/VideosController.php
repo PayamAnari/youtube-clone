d0aa1ceb50d385397847913db7349e7890cb1209
PayamAnari <anari.p62@gmail.com>
2024-01-28 21:33:36 +0100

    Add destroy func

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -52,8 +52,19 @@ class VideosController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Video $video)
+    public function destroy($id)
     {
-        //
+        $video = Video::find($id);
+
+        if (file_exists(public_path() . $video->video)) {
+            unlink(public_path() . $video->video);
+        }
+
+        if (file_exists(public_path() . $video->thumbnail)) {
+            unlink(public_path() . $video->thumbnail);
+        }
+
+        $video->delete();
+        return redirect()->route('deleteVideo');
     }
 }
