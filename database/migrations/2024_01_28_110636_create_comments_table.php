138d20ad2e7f755395e61c689aaa08037a747e54
PayamAnari <anari.p62@gmail.com>
2024-01-28 12:08:38 +0100

    Modify comment table

--- database/migrations/2024_01_28_110636_create_comments_table.php
+++ database/migrations/2024_01_28_110636_create_comments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('user');
+            $table->text('time');
+            $table->text('text');
             $table->timestamps();
         });
     }
